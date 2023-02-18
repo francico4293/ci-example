418aa099985207d357c03698159f15a85a0bdd8e
Colin Francis <francico@oregonstate.edu>
2023-02-18 15:17:15 -0500

    added test1

--- tests.py
+++ tests.py
@@ -1,4 +1,12 @@
 import unittest
+from task import my_func
+
+
+class TestCase(unittest.TestCase):
+    def test1(self):
+        expected = "Hello World"
+        self.assertEqual(my_func(), expected)
+
 
 if __name__ == "__main__":
     unittest.main(verbosity=2)
